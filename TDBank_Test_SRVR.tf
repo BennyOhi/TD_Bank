7b506646f595c73edaa6b01c7bff4429dfa17b41
Benedict Ohirhian <bohirhian@yahoo.com>
2022-07-17 16:46:01 -0400

    Initial commit :

---
+++ TDBank_Test_SRVR.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+    #profile = "default"
+               }
+
+resource "aws_instance" "TD_Bank_SRVR" {
+   ami = "ami-06640050dc3f556bb"
+   instance_type = "t2.micro"     
+                                 
+   tags = {
+   Name = "Test_Server"
+         }
+                                   }
+    
+ 
